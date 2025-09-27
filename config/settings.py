ea30405b5456e5ab2393e61ce78fb98893edadd4
Haohanmaiyami <kharitonovayan2018@gmail.com>
2025-09-27 02:06:18 -0400

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["84.252.139.98", "localhost", "127.0.0.1"]
 
 
 # Application definition
